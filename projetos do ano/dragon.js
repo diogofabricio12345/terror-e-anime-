949391b6cc6f56144a4f8300908713a5ea33b03f
diogofabricio12345 <128058085+diogofabricio12345@users.noreply.github.com>
2024-05-13 10:10:10 -0300

    Add files via upload

--- projetos do ano/dragon.js
+++ projetos do ano/dragon.js
@@ -1,11 +1,11 @@
 const botoes= document.querySelectorAll(".botao");
 const textos= document.querySelectorAll(".aba-conteudo");
 const contadores= document.querySelectorAll(".contador");
-const tempoObjetivo1= new Date("2024-10-05T00:00:00");
-const tempoObjetivo2= new Date("2024-10-05T00:00:00");
-contadores[0].text
-contadores[1].textContent
-const tempoAtual1= new Date;
+const TempoObjetivo1= new Date("2024-12-13T00:00:00");
+const TempoObjetivo2= new Date("2024-12-13T00:00:00");
+const TempoObjetivo3= new Date("2026-12-13T00:00:00");
+const TempoObjetivo4= new Date("2028-12-13T00:00:00");
+const tempoAtual= new Date;
 
 //Declara quais elementos vão ser lidos. O script vai ler todos que tiverem a classe "botao".
 
@@ -22,23 +22,45 @@ for(let i=0;i <botoes.length;i++){
         //Adiciona a classe ativo só no botão clicado.
     }
 }
-contadores[0].textContent= calculatempo(tempoObjetivo1);
-contadores[1].textContent= calculatempo(tempoObjetivo2);
-contadores[2].textContent= calculatempo(tempoObjetivo3);
-contadores[3].textContent= calculatempo(tempoObjetivo4);
+contadores[0].textContent=CalculaTempo(TempoObjetivo1);
+contadores[1].textContent=CalculaTempo(TempoObjetivo2);
+contadores[2].textContent=CalculaTempo(TempoObjetivo3);
+contadores[3].textContent=CalculaTempo(TempoObjetivo4);
 
+function CalculaTempo(TempoObjetivo){
+let tempoAtual=new Date();
+let tempoFinal=TempoObjetivo-tempoAtual;
+let segundos=Math.floor(tempoFinal/1000);
+let minutos=Math.floor(segundos/60);
+let horas=Math.floor(minutos/60);
+let dias=Math.floor(horas/24);
+let mes=Math.floor(dias/30);
+let anos=Math.floor(mes/12);
 
-function calculatempo(tempoObjetivo) {
-  let tempoAtual= new Date();
-  let tempoFinal= tempoObjetivo-tempoAtual;
-  let segundos= Math.floor(tempoFinal/1000);
-  let minutos= Math.floor(segundos/60);
-  let horas= Math.floor(minutos/60);
-  let dias= Math.floor(horas/24);
-  segundos%=60;
-  minutos%=60;
-  horas%=24;
-  return dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos";
- 
- 
+segundos%=60;
+minutos%=60;
+horas%=24;
+dias%=30;
+mes%=12;
+return anos+"anos "+mes+"mes "+dias+"dias "+horas+"horas "+minutos+"minutos "+segundos+"segundos ";
+
 }
+comecaCronometros();
+
+function comecaCronometros(){
+atualizaCronometro();
+setInterval(atualizaCronometro,1000);
+
+}
+
+function atualizaCronometro(){
+
+contadores[0].textContent = CalculaTempo(TempoObjetivo1);
+contadores[1].textContent = CalculaTempo(TempoObjetivo2);
+contadores[2].textContent = CalculaTempo(TempoObjetivo3);
+contadores[3].textContent = CalculaTempo(TempoObjetivo4);
+
+
+}
+
+ 
